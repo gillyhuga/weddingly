920c610a2411f8c1dd4e022e60d01614543dd5c6
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-08-12 00:09:50 +0700

    footer dinamis

--- src/components/common/FooterBar/index.js
+++ src/components/common/FooterBar/index.js
@@ -1,33 +1,55 @@
 import React from "react";
 import { FaInstagram, FaTwitter, FaYoutube, FaTiktok } from "react-icons/fa";
 import Image from "next/image";
-import logofooter from "@/public/images/logofooter.png";
+import logo from "@/public/images/footer-logo.png";
+import Link from "next/link";
+
+const menu = [
+  {
+    name: "Instagram",
+    icons: FaInstagram,
+    href: "https://www.instagram.com/",
+  },
+  {
+    name: "Twitter",
+    icons: FaTwitter,
+    href: "https://twitter.com/",
+  },
+  {
+    name: "Youtube",
+    icons: FaYoutube,
+    href: "https://www.youtube.com/",
+  },
+  {
+    name: "Tiktok",
+    icons: FaTiktok,
+    href: "https://www.tiktok.com/",
+  },
+];
 
 const FooterBar = () => {
   return (
-    <div className="hero w-sceen min-h-[calc(75vh-4rem)] bg-[url('../../public/images/Footer.png')] bg-white">
+    <div className="hero w-sceen min-h-[calc(75vh-4rem)] bg-[url('../../public/images/footer-bg.png')] bg-white">
       <footer class="footer footer-center  ">
         <div class="grid grid-flow-col gap-4  "></div>
         <div>
           <div className="pt-28">
-            <Image src={logofooter} width={500} height={500} />
+            <Image src={logo} width={350} height={350} />
           </div>
           <div class="grid grid-flow-col gap-4">
-            <a className="rounded-full bg-secondary h-14 w-14 flex justify-center items-center">
-              <FaInstagram className="text-3xl h-7 w-7 text-white " />
-            </a>
-            <a className="rounded-full bg-secondary h-14 w-14 flex justify-center items-center">
-              <FaTwitter className="text-3xl h-7 w-7 text-white " />
-            </a>
-            <a className="rounded-full bg-secondary h-14 w-14 flex justify-center items-center">
-              <FaYoutube className="text-3xl h-7 w-7 text-white " />
-            </a>
-            <a className="rounded-full bg-secondary h-14 w-14 flex justify-center items-center">
-              <FaTiktok className="text-3xl h-7 w-7 text-white " />
-            </a>
+            {menu.map((item) => {
+              const Icon = item.icons;
+              return (
+                <Link href={item.href}>
+                  <a className="rounded-full bg-secondary h-14 w-14 flex justify-center items-center">
+                    <Icon className="text-3xl h-7 w-7 text-white " />
+                  </a>
+                </Link>
+              );
+            })}
           </div>
         </div>
-        <div className=" pb-12">
+        <div>
           <p>Weddingly Wedding Planner 2022</p>
         </div>
       </footer>
