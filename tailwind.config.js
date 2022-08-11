166e951acf0461bd7094c98d97292654738b3947
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-08-11 23:00:02 +0700

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,25 @@ module.exports = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "#E8D0C2", //pink-100
-          "primary-focus": '#EEDCD1', //pink-75
-          "primary-content": '#F3E7E0', //pink-50
+    themes: [{
+      mytheme: {
+        "primary": "#E8D0C2", //pink-100
+        "primary-focus": '#EEDCD1', //pink-75
+        "primary-content": '#F3E7E0', //pink-50
 
-          "secondary": "#A47D41", //brown-100
-          "secondary-focus": '#BB9D70', //brown-75
-          "secondary-content": '#D1BEA0', //brown-50
+        "secondary": "#A47D41", //brown-100
+        "secondary-focus": '#BB9D70', //brown-75
+        "secondary-content": '#D1BEA0', //brown-50
 
-          "base-content": '#495057',//grey-4
-          "base-300": '#ACB5BD', //grey-3
-          "base-200": '#DDE2E5', //brown-2
-          "base-100": '#F5F5F5', //brown-1
+        "base-content": '#495057', //grey-4
+        "base-300": '#ACB5BD', //grey-3
+        "base-200": '#DDE2E5', //grey-2
+        "base-100": '#F5F5F5', //grey-1
 
-          "error": '#EF40', //red
-          "yellow": '#F2F720', //yellow
-          "green": '#13C139', //green
-        },
+        "error": '#EF40', //red
+        "yellow": '#F2F720', //yellow
+        "green": '#13C139', //green
       },
-    ],
+    }, ],
   },
 }
