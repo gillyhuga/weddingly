f57cef48ce09e9091883259d96560bb9b573ccb3
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-08-12 01:05:51 +0700

    add content test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
+import Content from "@/components/pages/home/Content";
 import Hero from "@/components/pages/home/Hero";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Content />
     </div>
-  )
+  );
 }
