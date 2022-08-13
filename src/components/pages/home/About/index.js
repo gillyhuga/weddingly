4ccc45b934550aeea7bacd449c9544bfb2443722
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-08-13 17:15:58 +0700

    add landing page

---
+++ src/components/pages/home/About/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import about from "@/public/images/home-about.png";
+
+const About = () => {
+  return (
+    <>
+      <div class="hero min-h-screen bg-white">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <div className=" grid-cols-12">
+            <Image src={about} alt="" width={900} height={900} />
+          </div>
+          <div>
+            <a class="rounded-full ... bg-primary font-bold px-7 p-2">
+              ABOUT WEDDINGLY
+            </a>
+            <h1 class="text-3xl font-bold pt-6">
+              The place to create the best wedding plan and make your dream
+              wedding possible.
+            </h1>
+            <p class="py-6">
+              The simple website to plan your perfect wedding and make it easy:
+              create your plan wedding, manage your budget, and organize your
+              guest list.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
