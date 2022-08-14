56f6f0bcbbca10998de0ee6cfcf92bc1e3437242
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2022-08-14 22:07:24 +0700

    Menambahkan modal dan alert, Memperbaiki navbar

--- src/components/layout/BaseLayout/index.js
+++ src/components/layout/BaseLayout/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import NavigationBar from "@/components/common/NavigationBar";
+import Modal from "@/components/common/Modal";
 import FooterBar from "@/components/common/FooterBar";
+import Alert from "@/components/common/Alert";
 
 const Layout = ({ children }) => {
   return (
     <>
       <div>
         <NavigationBar />
+        <Modal title="Log Out" desc="Are you sure?" action="Log Out" />
         <main>{children}</main>
+        <Alert alert="Save Changes" />
         <FooterBar />
       </div>
     </>
