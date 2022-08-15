6f73dd76218d3f5930fe0fbe158f49f19a75a1d8
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2022-08-15 19:54:10 +0700

    Menambahkan page/guest

--- src/components/common/Modal/index.js
+++ src/components/common/Modal/index.js
@@ -3,16 +3,16 @@ import React from 'react';
 const Modal = ({ title, desc, action }) => {
     return (
         <>
-            <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+            <input type="checkbox" id="my-modal" className="modal-toggle" />
             <div className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box text-center">
                     <h3 className="font-bold text-xl">{title}</h3>
                     <p className="py-4 text-lg">{desc}</p>
                     <div className="modal-action justify-center gap-2">
-                        <label htmlFor="my-modal-6" className="btn btn-error rounded-full text-white tracking-wider px-10">
+                        <label htmlFor="my-modal" className="btn btn-error rounded-full text-white tracking-wider px-10">
                             {action === "Log Out" ? "Log Out" : action}
                         </label>
-                        <label htmlFor="my-modal-6" className="btn btn-success rounded-full text-white tracking-wider px-10">Cancel</label>
+                        <label htmlFor="my-modal" className="btn btn-success rounded-full text-white tracking-wider px-10">Cancel</label>
                     </div>
                 </div>
             </div>
--- src/components/layout/BaseLayout/index.js
+++ src/components/layout/BaseLayout/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import NavigationBar from "@/components/common/NavigationBar";
+import NavigationBar from "@/components/common/NavigationBarLogin";
 import Modal from "@/components/common/Modal";
-import FooterBar from "@/components/common/FooterBar";
 import Alert from "@/components/common/Alert";
+import FooterBar from "@/components/common/FooterBar";
 
 const Layout = ({ children }) => {
   return (
     <>
       <div className="bg-white">
         <NavigationBar />
+        <Modal title="Log Out" desc="Are you sure?" action="Log Out" />
         <main>{children}</main>
+        {/* <Alert alert="Save Changes" /> */} {/* jika ingin menampilkan alert */}
         <FooterBar />
       </div>
     </>
