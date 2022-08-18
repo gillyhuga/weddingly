883aeafdc052f9e684a3eb396e83b150085a58cf
Gilly <gilly.huga@yahoo.co.id>
2022-08-19 01:30:12 +0700

    change: base url api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL_API = "http://stark-sea-10917.herokuapp.com/api";
+export const BASE_URL_API = "https://stark-sea-10917.herokuapp.com/api";
 
 export const LOGIN = BASE_URL_API + "/auth/login";
 export const REGISTER = BASE_URL_API + "/auth/register";
